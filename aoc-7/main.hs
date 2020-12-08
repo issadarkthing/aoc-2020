64dba037075d3ebce52a7c61dcc9c33711fcf89f
raziman <raziman.terra@gmail.com>
2020-12-08 22:53:48 +0800

    clean up day 7

--- aoc-7/main.hs
+++ aoc-7/main.hs
@@ -2,10 +2,12 @@ import Data.List.Split
 import Data.List ((\\))
 import Data.Maybe (fromJust)
 
+type Rule = (String, [(String, Int)])
+
 readInt :: String -> Int
 readInt = read
 
-parse :: String -> [(String, [(String, Int)])]
+parse :: String -> [Rule]
 parse = map makeBag . lines
 
 dropLast :: Int -> [a] -> [a]
@@ -13,7 +15,7 @@ dropLast i xs = take (length xs - i) xs
 
 -- pale cyan bags contain 2 posh black bags, 4 wavy gold bags, 2 vibrant brown bags.
 -- vibrant blue bags contain no other bags.
-makeBag :: String -> (String, [(String, Int)])
+makeBag :: String -> Rule
 makeBag str = (name, content)
     where (front:back:_) = splitOn " contain " str
           name = dropLast 5 front
@@ -22,24 +24,24 @@ makeBag str = (name, content)
                        then []
                        else map (getContent . words) $ splitOn ", " back
 
-findRule :: String -> [(String, [(String, Int)])] -> [(String, Int)]
+findRule :: String -> [Rule] -> [(String, Int)]
 findRule name = fromJust . lookup name
 
-contains :: [(String, [(String, Int)])] -> String -> String -> Bool
+contains :: [Rule] -> String -> String -> Bool
 contains rules target name = 
     if target == name 
         then True 
         else any (contains rules target . fst) bags
     where bags = findRule name rules
 
-part1 :: [(String, [(String, Int)])] -> Int
+part1 :: [Rule] -> Int
 part1 input = length $ filter (contains input "shiny gold" . fst) $ 
     filter ((/=) "shiny gold" . fst) input
 
-part2 :: [(String, [(String, Int)])] -> Int
+part2 :: [Rule] -> Int
 part2 input = pred $ countBags input ("shiny gold", 1)
 
-countBags :: [(String, [(String, Int)])] -> (String, Int) -> Int
+countBags :: [Rule] -> (String, Int) -> Int
 countBags rules (name, count) = count * (1 + totalBags)
     where bags = findRule name rules
           totalBags = sum $ map (countBags rules) bags
